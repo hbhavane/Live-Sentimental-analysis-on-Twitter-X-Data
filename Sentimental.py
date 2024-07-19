2e1700de10881f0c90deaf5bc1cd42dd86c5c491
Harshal Bhavane <78750775+hbhavane@users.noreply.github.com>
2024-07-19 12:59:40 -0400

    Update Sentimental.py

--- Sentimental.py
+++ Sentimental.py
@@ -161,7 +161,7 @@ def main():
             c = len(df[df["Sentiment"] == "Neutral"])
             d = np.array([a, b, c])
             explode = (0.1, 0.0, 0.1)
-            plt.figure(figsize=(3, 3))  # Reduced size
+            plt.figure(figsize=(1, 1))  
             plt.pie(d, shadow=True, explode=explode, labels=["Positive", "Negative", "Neutral"], autopct='%1.2f%%')
             st.pyplot()
 
